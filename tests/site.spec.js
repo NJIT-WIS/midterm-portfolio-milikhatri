b05f050e223023db19c2d1fed21efdbdc7107f37
“Mili <mk237@njit.edu>
2023-11-02 12:34:27 -0400

    Bug fixing playwright tests

--- tests/site.spec.js
+++ tests/site.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-const websiteURL = 'http://localhost:3000';
+const websiteURL = 'http://localhost:3000/index.html';
 
 const expectedLogoText = "MK";
 const expectedMenuItemCount = 3;
@@ -19,34 +19,41 @@ test('Check Logo in NavBar', async ({ page }) => {
 });
 
 test('Check Navigation Menu in NavBar', async ({ page }) => {
+  await page.goto('index.html');
   const menuItemCount = await page.locator('.menu .menu-item').count();
   expect(menuItemCount).toBe(expectedMenuItemCount);
 });
 
 test('Check Image Content', async ({ page }) => {
+  await page.goto('index.html');
   const imageCount = await page.locator('.image-content img').count();
   expect(imageCount).toBe(expectedImageCount);
 });
 
 test('Check Hero Title', async ({ page }) => {
+  await page.goto('index.html');
   expect(await page.locator('.hero h1').textContent()).toBe(expectedHeroTitle);
 });
 
 test('Check Hero Text', async ({ page }) => {
-expect(await page.locator('.hero .sub-text').textContent()).toBe(expectedHeroSubText);
+  await page.goto('index.html');
+  expect(await page.locator('.hero .sub-text').textContent()).toBe(expectedHeroSubText);
 });
 
 test('Check Links in Hero', async ({ page }) => {
+  await page.goto('index.html');
   const linksCount = await page.locator('.hero .links a').count();
   expect(linksCount).toBe(expectedHeroLinksCount);
 });
 
 test('Check Link in Hero Section', async ({ page }) => {
+  await page.goto('index.html');
   const linkCount = await page.locator('.hero a').count();
   expect(linkCount).toBe(expectedHeroLinkCount);
 });
 
 test('Test Email Input and Submit Button', async ({ page }) => {
+  await page.goto('index.html');
   const emailInput = await page.locator('.emailIn');
   const submitButton = await page.locator('.button');
   expect(await emailInput.isVisible()).toBeTruthy();
@@ -54,6 +61,7 @@ test('Test Email Input and Submit Button', async ({ page }) => {
 });
 
 test('Check Copyright Notice in Footer', async ({ page }) => {
+  await page.goto('index.html');
   expect(await page.locator('footer p').textContent()).toBe(expectedCopyrightText);
 });
 
